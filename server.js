fe385e70be0789f9a4a723b7e56bd42483ee0c68
Muhammad Ahmad Javed <ahmadjaved7101@gmail.com>
2026-04-02 00:24:31 +0500

    Change SMTP port to 465 to fix MailServer network timeout

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.use(express.static(path.join(__dirname, '')));
 // Nodemailer transport
 const transporter = nodemailer.createTransport({
     host: 'mail.xdialnetworks.com',
-    port: 587,
-    secure: false, // true for 465, false for other ports
+    port: 465,
+    secure: true, // true for 465, false for other ports
     auth: {
         user: 'info@adrollinc.com',
         pass: process.env.SMTP_PASS
